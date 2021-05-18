8ce84827ebf4541cd530f3c69d176f11f90ac732
Roberto Sousa <robertosousa186@gmail.com>
2021-05-17 21:59:59 -0300

    creating heroFactory to manipulate the data

---
+++ src/factories/heroFactory.js
@@ -0,0 +1,21 @@
+// Cria instancias para manipular dados do sistema
+const HeroRepository = require("./../repositories/heroRepository");
+const HeroService = require("./../services/heroService");
+
+const { join } = require("path");
+const filename = join(__dirname, "../../database", "data.json");
+
+const generateInstance = () => {
+  const heroRepository = new HeroRepository({
+    file: filename,
+  });
+
+  const heroService = new HeroService({
+    heroRepository,
+  });
+  return heroService;
+};
+
+module.exports = { generateInstance };
+
+generateInstance().find().then(console.log);
