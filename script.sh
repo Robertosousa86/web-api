b0779302539a9b6069a22380a94ee43e3646427c
Roberto Sousa <robertosousa186@gmail.com>
2021-05-19 00:57:39 -0300

    creating script for post method

--- script.sh
+++ script.sh
@@ -1,10 +1,15 @@
-echo '\n\n Request all heroes'
+echo '\n\nRequest all heroes'
 curl localhost:3000/heroes
 
-echo ' \n\n Request by id'
+echo ' \n\nRequest by id'
 curl localhost:3000/heroes/1
 
-echo '\n\n Resquest with whong body'
+echo '\n\nResquest with whong body'
 curl --silent -X POST \
 --data-binary '{"invalid": "data"}' \
+localhost:3000/heroes
+
+echo '\n\ncreating Leão'
+curl --silent -X POST \
+--data-binary '{"name": "Leao", "age": 100, "attribute": "intelligence"}' \
 localhost:3000/heroes
