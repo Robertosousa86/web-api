ea6c833637a7a5bfae2c0396a8558780ee990799
Roberto Sousa <robertosousa186@gmail.com>
2021-05-19 00:06:43 -0300

    creating script for bad request

--- script.sh
+++ script.sh
@@ -3,3 +3,8 @@ curl localhost:3000/heroes
 
 echo ' \n\n Request by id'
 curl localhost:3000/heroes/1
+
+echo '\n\n Resquest with whong body'
+curl --silent -X POST \
+--data-binary '{"invalid": "data"}' \
+localhost:3000/heroes
